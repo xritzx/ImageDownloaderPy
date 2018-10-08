98e89f72ffc7ab6a35aa49e1aa313aae4e09fbcb
Ritankar <ritankarpaul47@gmail.com>
2018-10-08 21:00:36 +0530

    Modified: Stable release

--- ImageDownloader.py
+++ ImageDownloader.py
@@ -2,31 +2,18 @@ import os
 import urllib.request as ur
 import sys
 import bs4 as bs
-
-def download():
-    imtype=input("Enter the Name of the folder to save image ")
-    url=input("Enter the url you want to scrap images from : ")
-    headers={}
-    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-    req=ur.Request(url,headers=headers)
-    res=ur.urlopen(req).read()
-    soup=bs.BeautifulSoup(res,"lxml")
-    user=os.getlogin()
-    os.chdir("C://Users//"+user+"//Pictures")
-    try:
-        os.mkdir(imtype)
-    except:
-        pass
-    os.chdir(imtype)
-    i=0
+def write_image(response):
+    soup = bs.BeautifulSoup(response ,"lxml")
+    i = 0
     for img in soup.find_all('img'):
-        imglink=img.get('src')
-        if(imglink=='None'):
-            imglink=img.get('data-src')
+        try:
+            imglink = img.get('src')
+        except:
+            imglink = img.get('data-src')
         if(str(imglink)!='None'):
             try:
-                name='cow'+str(i)+'.jpg'
-                f=open(name,'wb')
+                name = "Image_"+str(i)+'.jpg'
+                f = open(name,'wb')
                 f.write(ur.urlopen(str(imglink)).read())
                 f.close()
             except:
@@ -35,7 +22,21 @@ def download():
             i+=1
 
 def main():
-    download()
+    imtype = input("Enter the Name of the folder to save image ")
+    url = input("Enter the url you want to scrap images from : ")
+    os.chdir(os.path.dirname(os.path.abspath('__file__')))
+    try:
+        os.mkdir(imtype)
+    except:
+        pass
+    os.chdir(imtype)
+
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    req = ur.Request(url,headers=headers)
+    res = ur.urlopen(req).read()
+    write_image(res)
+
     
-if __name__=="__main__" :
+if __name__ == "__main__" :
     main()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ImageDownloaderPy
-Hello Friends chai pilo
+#Image Scrapper
+Who doesn't want preprocessed data ready to be crunched by you cool ML Algorithm.
+Since Data Collection is really tedious I've made a simple yet powerful solution to gather all your image data.
+A simple Python Image Scrapper for downloading all images in a single webpage.
+Just head to the webpage you want to Scrap and copy the link to this Script and it will do your job.
